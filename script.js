c409fa088c615d42fc4d3e1a01a9f03eb349b332
eva.wang <eva.wang81@gmail.com>
2022-03-31 18:49:10 +0800

    added start&stop function

--- script.js
+++ script.js
@@ -16,3 +16,23 @@ containerDiv.className = "container";
 
 containerDiv.append(displayDiv, startAndStopBtn, lapAndResetBtn);
 document.body.append(containerDiv);
+
+let lapCounter = 0;
+const lapData = [];
+let clickCounter = 0;
+let milliseconds = 0;
+const delayInMilliseconds = 1;
+
+startAndStopBtn.addEventListener("click", () => {
+  clickCounter += 1;
+  startAndStopBtn.innerText = "Stop";
+  const onTimer = setInterval(() => {
+    displayDiv.innerHTML = milliseconds;
+    if (clickCounter % 2 === 0) {
+      clearInterval(onTimer);
+      startAndStopBtn.innerText = "Start";
+    }
+    milliseconds += 1;
+  }, delayInMilliseconds);
+  console.log(milliseconds);
+});
