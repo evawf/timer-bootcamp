e7ae7e059029a9a89383aa0d147a2346706f20f2
eva.wang <eva.wang81@gmail.com>
2022-03-31 15:04:29 +0800

    added btns

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 // Please implement exercise logic here
+
+const displayDiv = document.createElement("div");
+displayDiv.className = "display";
+displayDiv.innerHTML = "Timer";
+// Add buttons
+const startAndStopBtn = document.createElement("button");
+startAndStopBtn.className = "btn";
+startAndStopBtn.innerText = "Start";
+const lapAndResetBtn = document.createElement("button");
+lapAndResetBtn.className = "btn";
+lapAndResetBtn.innerText = "Lap";
+
+const containerDiv = document.createElement("div");
+containerDiv.className = "container";
+
+containerDiv.append(displayDiv, startAndStopBtn, lapAndResetBtn);
+document.body.append(containerDiv);
