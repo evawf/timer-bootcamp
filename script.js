3a6d72aae0c3dfb0b3269ae2adb3cf835ae79d7e
eva.wang <eva.wang81@gmail.com>
2022-04-01 10:19:31 +0800

    added lap function

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-// Please implement exercise logic here
+const elapsedTime = document.createElement("div");
+elapsedTime.className = "display";
+elapsedTime.innerHTML = "Timer";
 
-const displayDiv = document.createElement("div");
-displayDiv.className = "display";
-displayDiv.innerHTML = "Timer";
+const lapsInfoDiv = document.createElement("div");
+lapsInfoDiv.className = "lapsInfo";
 // Add buttons
 const startAndStopBtn = document.createElement("button");
 startAndStopBtn.className = "btn";
@@ -10,24 +11,45 @@ startAndStopBtn.innerText = "Start";
 const lapAndResetBtn = document.createElement("button");
 lapAndResetBtn.className = "btn";
 lapAndResetBtn.innerText = "Lap";
+lapAndResetBtn.disabled = true;
 
 const containerDiv = document.createElement("div");
 containerDiv.className = "container";
 
-containerDiv.append(displayDiv, startAndStopBtn, lapAndResetBtn);
-document.body.append(containerDiv);
+containerDiv.append(elapsedTime, startAndStopBtn, lapAndResetBtn);
+document.body.append(containerDiv, lapsInfoDiv);
 
 let lapCounter = 0;
-const lapData = [];
+const lapsData = [];
 let clickCounter = 0;
 let milliseconds = 0;
 const delayInMilliseconds = 1;
 
-startAndStopBtn.addEventListener("click", () => {
+function padTo2Digits(num) {
+  return num.toString().padStart(2, "0");
+}
+
+const convertMsToHM = (milliseconds) => {
+  let seconds = Math.floor(milliseconds / 1000);
+  let minutes = Math.floor(seconds / 60);
+  let ms = milliseconds % 100;
+  seconds = ms >= 100 ? seconds : seconds;
+  seconds = seconds % 60;
+  minutes = seconds >= 30 ? minutes : minutes;
+  minutes = minutes % 60;
+
+  return `${padTo2Digits(minutes)}:${padTo2Digits(seconds)}:${padTo2Digits(
+    ms
+  )}`;
+};
+
+const onTimer = () => {
   clickCounter += 1;
   startAndStopBtn.innerText = "Stop";
+  lapAndResetBtn.disabled = false;
+
   const onTimer = setInterval(() => {
-    displayDiv.innerHTML = milliseconds;
+    elapsedTime.innerHTML = convertMsToHM(milliseconds);
     if (clickCounter % 2 === 0) {
       clearInterval(onTimer);
       startAndStopBtn.innerText = "Start";
@@ -35,4 +57,28 @@ startAndStopBtn.addEventListener("click", () => {
     milliseconds += 1;
   }, delayInMilliseconds);
   console.log(milliseconds);
+  return milliseconds;
+};
+
+const displayLapsInfo = (laps) => {
+  console.log(laps);
+  let displayLaps = "";
+  for (let i = 0; i < laps.length; i += 1) {
+    displayLaps += `Lap ${laps[i].lap}:  ${convertMsToHM(laps[i].time)}<br>`;
+  }
+  return displayLaps;
+};
+
+startAndStopBtn.addEventListener("click", onTimer);
+lapAndResetBtn.addEventListener("click", () => {
+  lapCounter += 1;
+  console.log(milliseconds);
+
+  let lapTime = onTimer();
+  let lapData = {
+    lap: lapCounter,
+    time: lapTime,
+  };
+  lapsData.push(lapData);
+  lapsInfoDiv.innerHTML = displayLapsInfo(lapsData);
 });
